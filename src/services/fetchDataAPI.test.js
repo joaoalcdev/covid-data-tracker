5fded7677b614220cdad0cf2c01f5f829cf37450
João Alcântara <joaosantalcantara@gmail.com>
2022-02-09 10:55:45 -0300

    fix fetchDataAPI tests

--- src/services/fetchDataAPI.test.js
+++ src/services/fetchDataAPI.test.js
@@ -2,25 +2,26 @@ const fetchDataFromAPI = require('./fetchDataAPI')
 const confirmDate = require('../helpers/confirmDate')
 
 describe('Data from the public API of "Our World In Data"', () => {
-  // test('yesterday cases greater than 0', async () => {
-  //   const data = await fetchDataFromAPI()
-  //   const yesterdayDate = confirmDate(new Date())
+  test('yesterday cases greater than 0', async () => {
+    const data = await fetchDataFromAPI(new Date('2022/02/08'))
+    const yesterdayDate = confirmDate(new Date())
 
-  //   const yesterday = data.find(data => data.date === yesterdayDate)
-  //   const { new_cases: yesterdayCases } = yesterday
+    const yesterday = data.find(data => data.date === yesterdayDate)
+    const { new_cases: yesterdayCases } = yesterday
 
-  //   expect(yesterdayCases).toBeGreaterThan(0)
-  // })
+    expect(yesterdayCases).toBeGreaterThan(0)
+  })
 
-  test('✅ OK | 04/12/2021 cases is equal to 690431', async () => {
+  test('✅ OK | 2022/02/08 cases is equal to 2239959', async () => {
+    // Date = YYYY/MM/DD
     const data = await fetchDataFromAPI()
-    const date = confirmDate(new Date('04/12/2021'))
+    const date = confirmDate(new Date('2022/02/08'))
 
     const day = data.find(data => data.date === date)
     const { new_cases: cases } = day
 
-    expect(cases).toEqual(690431)
-    // Ok ✅ 
+    expect(cases).toEqual(2239959)
+    // Ok ✅
   })
-  jest.setTimeout(12000)
+  jest.setTimeout(20000)
 })
