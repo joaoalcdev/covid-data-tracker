30098fa4e95610b1a045adbefc661dff8669723f
João Alcântara <joaosantalcantara@gmail.com>
2022-02-09 10:04:56 -0300

    fix confirmDate test

--- src/helpers/confirmDate.test.js
+++ src/helpers/confirmDate.test.js
@@ -5,15 +5,12 @@ describe('formatted date from 1 day before: (YYYY-MM-DD) = {(yesterday) - 1}', (
     expect(confirmDate(new Date('10-17-2021'))).toBe('2021-10-16')
     // Ok ✅
   })
-
   test('when send giving 08/11/2021, returning 08/10/2021 formatted date', () => {
     expect(confirmDate(new Date('08/11/2021'))).toBe('2021-08-10')
     // Ok ✅
   })
-
   test('when send giving 02/15/2021 returning 02/14/2021 date', () => {
     expect(confirmDate(new Date('02/15/2021'))).toBe('2021-02-14')
     // Ok ✅
   })
-  
 })
