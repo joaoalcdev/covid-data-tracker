07f53c158abc1218072d13a03c582f0e12c1b4fb
João Alcântara <joaosantalcantara@gmail.com>
2022-02-09 08:56:10 -0300

    fix first test confirmDate

--- src/helpers/confirmDate.test.js
+++ src/helpers/confirmDate.test.js
@@ -3,5 +3,9 @@ const confirmDate = require('./confirmDate')
 describe('formatted date from 1 day before: (YYYY-MM-DD) = {(yesterday) - 1}', () => {
   test('return yesterday date', () => {
     expect(confirmDate(new Date('10-17-2021'))).toBe('2021-10-16')
+    // Ok ✅
   })
+
+  
+  
 })
