d249140ed7028b8e11da0f47b7ae76abf517bf54
João Alcântara <joaosantalcantara@gmail.com>
2022-02-09 08:37:41 -0300

    added predict days funcion

---
+++ src/controllers/predictTheNextDays.js
@@ -0,0 +1,42 @@
+const fetchDataFromAPI = require("../services/fetchDataFromAPI");
+
+const ensureDate = require("../helpers/confirmDate");
+
+async function ensureDaysToCome(days) {
+  const data = await fetchDataFromAPI();
+
+  const currentData = ensureDate(new Date());
+  // const currentData = "2021-08-01" fixed value;
+
+  try {
+    const index = data.findIndex((data) => data.date === currentData);
+
+    if (index === -1) {
+      throw new Error();
+    }
+
+    const { new_cases: currentDayNewCases } = data[index];
+
+    const yesterdayIndex = index - 1;
+    const { new_cases: yesterdayNewCases } = data[yesterdayIndex];
+
+    const dayBeforeYesterdayIndex = index - 2;
+    const { new_cases: dayBeforeYesterdayNewCases } =
+      data[dayBeforeYesterdayIndex];
+
+    for (let i = 1; i <= days; i++) {
+      const dayBeforeNewCases =
+        i >= days / 2 ? yesterdayNewCases : dayBeforeYesterdayNewCases;
+      const margin = 1 + 0.2 * Math.random();
+
+      const proportion = (currentDayNewCases / dayBeforeNewCases) * margin;
+      const newCases = Math.ceil(currentDayNewCases * proportion);
+
+      console.log(`${i} => ${newCases}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = ensureDaysToCome;
